e32bea351d075f07cdbda9b99b921dc6f9b7f913
Quad <amarm6493@halsey157.com>
2017-01-28 14:00:45 -0500

    Create pay.js

---
+++ kandy-stix/pay.js
@@ -0,0 +1,6 @@
+var 1 = document.getElementById("first").value;
+var 2 = document.getElementById("second").value;
+var 3 = document.getElementById("third").value;
+function Submit() {
+  window.alert("First Layer:" + 1\n "Second Layer:" + 2\n "Third Layer" + 3\n);
+}
